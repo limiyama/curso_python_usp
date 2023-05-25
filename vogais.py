17d84f3dadba9d6ee4e3ebdfffb05bf223924216
mimiyama <lmizuyama@gmail.com>
2023-05-25 11:54:18 -0300

    vogal

--- vogais.py
+++ vogais.py
@@ -1,11 +1,6 @@
-b = input("digite")
-letra = b.lower()
-
-def vogal(a):
-    vogais = ["a", "e", "i", "o", "u"]
-    if any(element in a for element in vogais):
-        return
+def vogal (v):
+    letra = v.lower()
+    if letra in "aeiou":
+        return True
     else:
-        return ("False")
-    
-vogal(letra)
+        return False
